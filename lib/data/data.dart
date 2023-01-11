e61cea6c7ed6c113ca2e34dac7c7d3033b99ad64
Atuoha <atuohainitiatives@gmail.com>
2023-01-11 11:40:45 +0100

    full data setup

---
+++ lib/data/data.dart
@@ -0,0 +1,64 @@
+import '../model/data.dart';
+
+List<Doctor> doctors = [
+  Doctor(
+    name: 'John Philip',
+    imgUrl: 'imgs/1.png',
+    jobTitle: 'Cardiac sonographer',
+  ),
+  Doctor(
+    name: 'Isaac Matthew',
+    imgUrl: 'imgs/2.png',
+    jobTitle: 'Cytogenetic technologist',
+  ),
+  Doctor(
+    name: 'Anthony Enoch',
+    imgUrl: 'imgs/3.png',
+    jobTitle: 'Dietitian',
+  ),
+  Doctor(
+    name: 'Dave Whyte',
+    imgUrl: 'imgs/4.png',
+    jobTitle: 'Genetic counselor',
+  ),
+  Doctor(
+    name: 'Peter Trug',
+    imgUrl: 'imgs/5.png',
+    jobTitle: 'Medical physicist',
+  ),
+  Doctor(
+    name: 'Zack Bently',
+    imgUrl: 'imgs/6.png',
+    jobTitle: 'Nurse',
+  ),
+  Doctor(
+    name: 'Vivian Newton',
+    imgUrl: 'imgs/7.png',
+    jobTitle: 'Orthoptist',
+  ),
+  Doctor(
+    name: 'Frank Ferrari',
+    imgUrl: 'imgs/8.png',
+    jobTitle: 'Pharmacist',
+  ),
+  Doctor(
+    name: 'Mark Lurther',
+    imgUrl: 'imgs/9.png',
+    jobTitle: 'Radiation therapist',
+  ),
+  Doctor(
+    name: 'Stella Dav',
+    imgUrl: 'imgs/10.png',
+    jobTitle: 'Surgical technologist',
+  ),
+  Doctor(
+    name: 'Gary Yale',
+    imgUrl: 'imgs/11.png',
+    jobTitle: 'Respiratory therapist',
+  ),
+  Doctor(
+    name: 'Theo Sandra',
+    imgUrl: 'imgs/12.png',
+    jobTitle: 'Perioperative nurse',
+  ),
+];
---
+++ lib/model/data.dart
@@ -0,0 +1,11 @@
+class Doctor {
+  final String name;
+  final String imgUrl;
+  final String jobTitle;
+
+  Doctor({
+    required this.name,
+    required this.imgUrl,
+    required this.jobTitle,
+  });
+}
