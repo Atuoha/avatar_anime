1d66e5fd7914512703698bfe3828430e40d2b938
Atuoha <atuohainitiatives@gmail.com>
2023-01-11 11:03:32 +0100

    init

---
+++ android/app/src/main/kotlin/com/example/avatar_anime/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.avatar_anime
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:avatar_anime/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
